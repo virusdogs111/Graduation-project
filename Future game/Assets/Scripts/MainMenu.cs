046a1eb147c172da18eaf7731efeb8d3382cd699
Евгений <37345286+virusdogs111@users.noreply.github.com>
2018-03-14 12:40:18 +0600

    Graduation project

---
+++ Future game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Linq;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour {
+
+    public Dropdown resolutionsDropdown;//список разрешений экрана
+    Resolution[] resolutions;
+    public Dropdown qualityDropdown;//список качество графики
+    public AudioMixer audioMixer;//миксер главного звука
+
+
+
+    void Start()
+    {
+        //разрешение экрана
+        resolutions = Screen.resolutions;
+        resolutionsDropdown.ClearOptions();
+
+        List<string> options = new List<string>();      
+        int currenrResolutionValue = 0;
+        string isOption = "";
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            if (isOption == option) continue;
+            isOption = option; 
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currenrResolutionValue = i;
+            }
+        }
+       
+        resolutionsDropdown.AddOptions(options);
+        resolutionsDropdown.value = currenrResolutionValue;
+        resolutionsDropdown.RefreshShownValue();
+        //конец
+
+        //список качество графики
+        qualityDropdown.ClearOptions();
+        qualityDropdown.AddOptions(QualitySettings.names.ToList());
+        qualityDropdown.value = QualitySettings.GetQualityLevel();
+        //конец
+    }
+//Меню настроек
+
+        public void SetResolution(int resotionIndex)
+    {
+        Resolution resolution = resolutions[resotionIndex];
+        Screen.SetResolution(resolution.width,resolution.height,Screen.fullScreen);
+    }
+
+    public void SetSound(float sound)
+    {
+        audioMixer.SetFloat("sound", sound);
+    }
+  
+    public void SetQualityLevel()
+    {
+        QualitySettings.SetQualityLevel(qualityDropdown.value);
+    }
+
+    public void ShowSettings(GameObject obj)
+    {
+        obj.SetActive(true);
+    }
+
+    public void HideSettings(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+
+    public void FullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+ //Конец
+
+ //Главное меню
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadGame()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+//Конец
+
+}
---
+++ Future game/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    private bool IsPausePanel = false;
+    [SerializeField]
+    private GameObject pausePanel;
+    [SerializeField]
+    private GameObject person;
+    private FirstPersonController personScript;
+    public KeyCode PauseButton;
+
+	
+	void Start () {
+        personScript = person.GetComponent<FirstPersonController>();
+	}
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(PauseButton) && !IsPausePanel)
+        {
+            pausePanel.SetActive(true);
+            personScript.enabled = false;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            Time.timeScale = 0;
+            IsPausePanel = true;
+        }
+
+        else if (Input.GetKeyDown(PauseButton) && IsPausePanel)
+        {
+            IsPausePanel = false;
+            pausePanel.SetActive(false);
+            personScript.enabled = true;
+            Time.timeScale = 1;
+        }
+    }
+
+        public void Continue()
+    {
+        IsPausePanel = false;
+        pausePanel.SetActive(false);
+        personScript.enabled = true;
+        Time.timeScale = 1;
+    }
+
+         public void Menu()
+    {
+        IsPausePanel = false;
+        pausePanel.SetActive(false);
+        personScript.enabled = true;
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+
+         public void Quit()
+    {
+        Application.Quit();
+    } 
+   
+}
